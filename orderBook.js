d5dc54b1780e3054b6b4c96d17b7018365641e77
Alexander Le <alexanderle0817@gmail.com>
2019-06-26 22:00:30 -0400

    Clarify variables that add incoming order to book

--- orderBook.js
+++ orderBook.js
@@ -1,5 +1,5 @@
-// Check if the book has 0 orders and adds the incoming order in if so
-function newOrder(existingBook,incomingOrder) {
+//Adds the incoming order to the existing book
+function addedOrderToBook(existingBook,incomingOrder) {
   return [...existingBook, incomingOrder]
 }
 
@@ -8,19 +8,25 @@ function checkForMatchingOrder(existingBook, incomingOrder) {
   var differentTypesBook = existingBook.filter(order => order.type !== incomingOrder.type)
   if (differentTypesBook.length === 0) {
     //console.log('Oh hey theres no opposite order so well add the incoming order to the book')
-    return [...existingBook, incomingOrder]
+    return addedOrderToBook(existingBook,incomingOrder)
   }
   else {
-    // There is at least 1 order that can potentially be fulfilled. Check further if it can be
-    existingBook.map(function(order) {
-      // If quantity and price do not match between the incoming order and existing orders, add incoming order to the book
-      if (order.type !== incomingOrder.type && order.quantity !== incomingOrder.quantity && order.price !== incomingOrder.quantity) {
-        existingBook = [...existingBook, incomingOrder]
+    // There is at least 1 order that can potentially be fulfilled.
+    var potentialMatchingOrders = 0
+    existingBook.forEach(function(order) {
+      // If quantity and price match between the incoming order and existing orders, count it and fulfill it somehow
+      if (order.type !== incomingOrder.type && order.quantity === incomingOrder.quantity && order.price === incomingOrder.quantity) {
+        potentialMatchingOrders += 1
       }
+      
     })
+    // No matches means add incoming order to the book
+    if (potentialMatchingOrders === 0) {
+      return addedOrderToBook(existingBook,incomingOrder)
+    }
   }
   //console.log(existingBook)
-  return existingBook
+  //return existingBook
 }
 
 
@@ -28,7 +34,7 @@ export function reconcileOrder(existingBook, incomingOrder) {
   //throw new Error('Not Implemented Yet')
   var book
   if (!existingBook.length) {
-    book = newOrder(existingBook, incomingOrder)
+    book = addedOrderToBook(existingBook, incomingOrder)
   }
   else {
     book = checkForMatchingOrder(existingBook, incomingOrder)
